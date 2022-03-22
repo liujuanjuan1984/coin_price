9b6f30ee26220c9c4805fc2ea506dbab049fc35c
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-03-22 20:47:30 +0800

    detail

--- scripts/example_once.py
+++ scripts/example_once.py
@@ -3,7 +3,7 @@ from swap import SwapPrice
 from rumpyconfig import RumpyConfig
 
 bot = Bot(**RumpyConfig.GUI).init()
-# bot.once_post()
+bot.once_post()
 
 texts = SwapPrice().rum_rate()
 bot.group_id = "0be13ee2-10dc-4e3a-b3ba-3f2c440a6436"
